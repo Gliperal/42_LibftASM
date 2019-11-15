766844bd4d54cccb5da4e7f4ad6f6aad6232d1e7
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-11-14 19:28:37 -0800

    Updated ft_strcat and ft_strlen to use REP

--- ft_strcat.s
+++ ft_strcat.s
@@ -9,13 +9,9 @@ loop1_step:
 loop1_body:
 	cmp		byte [rdi], 0
 	jne		loop1_step
-	jmp		loop2_body
-loop2_step:
-	inc		rdi
-	inc		rsi
-loop2_body:
-	mov		dl, [rsi]
-	mov		[rdi], dl
-	cmp		byte [rdi], 0
-	jne		loop2_step
+	cld
+loop2:
+	movsb
+	cmp		byte [rdi - 1], 0
+	jne		loop2
 	ret
--- ft_strlen.s
+++ ft_strlen.s
@@ -2,11 +2,10 @@ global _ft_strlen
 
 section .text
 _ft_strlen:
-	mov		rax, -1
-loop:
-	inc		rax
-	mov		dl, byte [rdi]
-	inc		rdi
-	cmp		dl, 0
-	jne		loop
+	xor		rax, rax
+	mov		rcx, -1
+	cld
+	repne scasb
+	add		rcx, 2
+	sub		rax, rcx
 	ret
