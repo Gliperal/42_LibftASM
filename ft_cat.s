68cfac3001a8df99f7ae8cffb1c01cae3e113028
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-11-17 21:56:06 -0800

    ft_cat

---
+++ ft_cat.s
@@ -0,0 +1,30 @@
+global _ft_cat
+
+section .bss
+buffsize	equ 1024
+buff:		resb buffsize
+
+section .text
+_ft_cat:
+	; save fd for later use
+	; set rsi, which is the same for read and write
+	mov		r8, rdi
+	lea		rsi, [rel buff]
+loop:
+	; read into buff
+	mov		rdi, r8
+	mov		rdx, buffsize
+	mov		rax, 0x02000003
+	syscall
+	; check for read end / fail
+	cmp		rax, 0
+	jle		loop_end
+	; write from buff
+	mov		rdi, 1
+	mov		rdx, rax
+	mov		rax, 0x02000004
+	syscall
+	; repeat
+	jmp		loop
+loop_end:
+	ret
